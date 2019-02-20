fdf874b91eafc4bb78494b464757e7ff422ac005
krishnakeerthi <krishnakeerthi675@gmail.com>
2019-02-20 07:42:50 -0500

    Adding public space flights

---
+++ code.rb
@@ -0,0 +1,10 @@
+
+
+dehydrated_banana = 1.27
+tofu_cakes = 4.17
+cheese_spread = 3.79
+dehydrated_ice_cream_sandwich = 0.75
+
+puts tofu_cakes*2 + dehydrated_ice_cream_sandwich*2
+puts cheese_spread *8 + dehydrated_banana
+puts (dehydrated_banana + tofu_cakes + cheese_spread + dehydrated_ice_cream_sandwich)*3
